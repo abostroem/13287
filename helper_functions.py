f9359b2af6b568e13ad18b5d545b964faf040e47
Azalee Bostroem <abostroem@gmail.com>
2014-09-03 23:39:25 -0700

    moving get_repository_path and add_date_to_plot to helper_functiosn_file

---
+++ helper_functions.py
@@ -0,0 +1,11 @@
+def get_repository_path():
+	current_dir = os.getcwd()
+	users_path = current_dir.split('/13287')[0]
+	repo_path = os.path.join(users_path, '13287')
+	return repo_path
+
+def add_date_to_plot(ax):
+    today = datetime.today()
+    xlims = ax.get_xlim()
+    ylims = ax.get_ylim()
+    ax.text(xlims[1], -.06*(ylims[1] - ylims[0])+ylims[0], '{}-{}-{}'.format(today.year, today.month, today.day))
